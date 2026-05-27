7b2b01d314ea22d3dc54f3bae495db33a2ea923f
Gabriel Castro <castrovisionario@icloud.com>
2026-05-27 11:40:00 -0300

    feat: add user model initialization and user creation route with uuid

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import User from "./app/models/User.js";
+import { v4 } from "uuid";
+
+const routes = new Router();
+
+
+routes.get('/', async (req, res) => {
+
+    const user = {
+        id: v4(),
+        name: 'Henrique',
+        email: 'henrique@email.com',
+        password_hash: '123456',
+        admin: false
+    }
+
+    await User.create(user);
+
+    res.status(201).json(user);
+});
+
+export default routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import app from './app.js';
-import './config/database.js';
+import './config/database.cjs';
+import './database/index.js';
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
